c50632ab8ae06a953ebd51a464129235a3f8ddbe
RSL-Redstonier <ducky@kellerville.com>
2017-05-12 21:13:21 -0400

    Delete init.lua

--- init.lua
+++
@@ -1,8 +0,0 @@
-local path = minetest.get_modpath("skytest")
-dofile(path.."/Craft_Items.lua")
-dofile(path.."/Nodes.lua")
-dofile(path.."/tools.lua")
-dofile(path.."/leaf_press.lua")
-dofile(path.."/compost_bin.lua")
-dofile(path.."/infused_soil.lua")
-
